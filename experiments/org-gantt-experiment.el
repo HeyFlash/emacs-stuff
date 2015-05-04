04c29f1f36929575f65e59e5076d0c80d36e7cb1
unknown <schmitbd@pavane.visus.uni-stuttgart.de>
2015-05-04 18:05:40 +0200

    Working version with deadline/schedule

--- experiments/org-gantt-experiment.el
+++ experiments/org-gantt-experiment.el
@@ -38,21 +38,39 @@ of the given element. timestamp-type is either ':scheduled or ':deadline"
   (org-element-map element 'headline (lambda (subelement) subelement)
 		   nil nil 'headline))
 
-(defun org-gantt-tp< (tp1 tp2 key)
-  "Time property smaller, works with nil."
+(defun org-gantt-tp-compare (tp1 tp2 key compare-function)
+  "Time property compare, works with nil."
   (and (org-element-property key tp1)
        (or (not (org-element-property key tp2))
-	   (< (org-element-property key tp1) (org-element-property key tp2)))))
+	   (funcall compare-function (org-element-property key tp1) (org-element-property key tp2)))))
 
 (defun org-gantt-timestamp-smaller (ts1 ts2)
   "Returns true iff timestamp ts1 is before ts2"
   (and ts1
        (or (not ts2)
-	   (org-gantt-tp< ts1 ts2 ':year-start)
-	   (org-gantt-tp< ts1 ts2 ':month-start)
-	   (org-gantt-tp< ts1 ts2 ':day-start)
-	   (org-gantt-tp< ts1 ts2 ':hour-start)
-	   (org-gantt-tp< ts1 ts2 ':minute-start))))
+           ;;I'm sure that's not how it should be done...
+           (if (org-gantt-tp-compare ts1 ts2 ':year-start #'=)
+               (if (org-gantt-tp-compare ts1 ts2 ':month-start #'=)
+                   (if (org-gantt-tp-compare ts1 ts2 ':day-start #'=)
+                       (if (org-gantt-tp-compare ts1 ts2 ':hour-start #'=)
+                           (if (org-gantt-tp-compare ts1 ts2 ':minute-start #'=)
+                               (org-gantt-tp-compare ts1 ts2 ':minute-start #'<))
+                         (org-gantt-tp-compare ts1 ts2 ':hour-start #'<))
+                     (org-gantt-tp-compare ts1 ts2 ':day-start #'<))
+                 (org-gantt-tp-compare ts1 ts2 ':month-start #'<))
+             (org-gantt-tp-compare ts1 ts2 ':year-start #'<))
+
+
+           ;; (org-gantt-tp< ts1 ts2 ':year-start)
+           ;; (org-gantt-tp< ts1 ts2 ':month-start)
+           ;; (org-gantt-tp< ts1 ts2 ':day-start)
+           ;; (org-gantt-tp< ts1 ts2 ':hour-start)
+           ;; (org-gantt-tp< ts1 ts2 ':minute-start)
+           )))
+
+(defun org-gantt-timestamp-larger (ts1 ts2)
+  "Returns true iff not timestamp ts1 is before ts2"
+  (not (org-gantt-timestamp-smaller ts1 ts2)))
 
 (defun org-gantt-subheadline-extreme (element comparator time-getter)
   "Returns the smallest/largest timestamp of the subheadlines 
@@ -151,6 +169,15 @@ A gantt-info is a plist containing :name :start :end"
 	subelements
 	(concat prefix "  "))))))
 
+(defun org-gantt-get-extreme-date (data time-getter timestamp-comparer)
+  "Get the first or last date (depending on timestamp-comparer) 
+of all the timestamps in data"
+  (let ((timestamps
+         (org-element-map data 'headline 
+           (lambda (headline) 
+             (funcall time-getter headline)))))
+    (car (sort timestamps timestamp-comparer))))
+
 (defun org-gantt-info-list-to-pgfgantt (data &optional prefix)
   "Gets a list of gantt-info-objects.
 Returns a pgfgantt string representing that data."
@@ -159,9 +186,31 @@ Returns a pgfgantt string representing that data."
 
 (defun org-dblock-write:pgf-gantt-chart (params)
   "The function that is called for updating gantt chart code"
-  (insert
-   (concat
-    "\\begin{ganttchart}[time slot format=isodate]{2015-05-01>}{2015-06-30}\n"
-    (org-gantt-info-list-to-pgfgantt
-     (org-gantt-crawl-headlines (org-element-parse-buffer) nil nil nil))
-    "\\end{ganttchart}")))
+  (let* ((titlecalendar (plist-get params :titlecalendar))
+         (start-date (plist-get params :startdate))
+         (end-date (plist-get params :enddate))
+         (additional-parameters (plist-get params :parameters))
+         (parsed-data (org-element-parse-buffer))
+         (org-gantt-info-list (org-gantt-crawl-headlines parsed-data nil nil nil)))
+    (insert
+     (concat
+      "\\begin{ganttchart}[time slot format=isodate"
+      (when additional-parameters 
+        (concat ", " additional-parameters))
+      "]{" 
+      (or start-date
+          (org-gantt-timestamp-to-string 
+           (org-gantt-get-extreme-date parsed-data #'org-gantt-get-start-time #'org-gantt-timestamp-smaller)))
+      "}{"
+      (or end-date
+          (org-gantt-timestamp-to-string
+           (org-gantt-get-extreme-date parsed-data #'org-gantt-get-end-time #'org-gantt-timestamp-larger))) 
+      "}\n"
+      "\\gantttitlecalendar{"
+      (if titlecalendar
+          titlecalendar
+        "year, month=name, day")
+    " }\\\\\n"
+    (org-gantt-info-list-to-pgfgantt org-gantt-info-list)
+    "\\end{ganttchart}"))))
+  
